db351b4b1d9edf94f018eeb553cfb1e2192ee216
Richard Gagg <r.gagg@outlook.com>
2025-01-08 10:56:12 +1100

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Richard Gagg on 8/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Richard Gagg on 8/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
