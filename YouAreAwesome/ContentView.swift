f11af57b52322e3e1d91675074ae9fcab2eb534b
Richard Gagg <r.gagg@outlook.com>
2025-01-10 13:19:07 +1100

    After layout guide part 3

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,43 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var messageString = "I'm a programer!"
+  //  @State private var messageString = "I'm a programer!"
+  @State private var messageString = "When the genius bar needs help, they call you!"
+  var screenHeight: CGFloat { UIScreen.main.bounds.height }
+  var screenWidth: CGFloat { UIScreen.main.bounds.width }
   
   var body: some View {
-    VStack {
-      Spacer()
+    
+    ZStack {
       
-      Image(systemName: "swift")
-        .resizable()
-        .scaledToFit()
-        .foregroundStyle(.orange)
-        .frame(width: 200, height: 200)
-      
-      Text(messageString)
-        .font(.largeTitle)
-        .fontWeight(.ultraLight)
-        .foregroundStyle(.red)
-      
-      Spacer()
-      
-      HStack {
-        Button {
-          messageString = "Awesome!"
-        } label: {
-          Text("Awesome")
-        }
+      VStack() {
         
-        Button {
-          messageString = "Great!"
-        } label: {
-          Text("Great")
+        Spacer()
+        
+        Image(systemName: "swift")
+          .resizable()
+          .scaledToFit()
+          .foregroundStyle(.orange)
+          .frame(width: 200, height: 200)
+        
+        Text(messageString)
+          .font(.largeTitle)
+          .fontWeight(.ultraLight)
+          .minimumScaleFactor(0.5)
+          .multilineTextAlignment(.center)
+          .foregroundStyle(.red)
+        //        .frame(width: 300, height: 150)
+          .frame(height: 150)
+          .frame(maxWidth: .infinity)
+          .padding()
+        
+        Spacer()
+        
+        HStack {
+          Button {
+            messageString = "Awesome!"
+          } label: {
+            Text("Awesome")
+          }
+          
+          Spacer()
+          
+          Button {
+            messageString = "Great!"
+          } label: {
+            Text("Great")
+          }
         }
+        .buttonStyle(.borderedProminent)
+        .font(.title2)
+        .tint(.orange)
       }
-      .buttonStyle(.borderedProminent)
-      .font(.title2)
-      .tint(.orange)
+      .padding()
+      
     }
-    .padding()
   }
 }
 
