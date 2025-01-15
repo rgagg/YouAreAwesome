29ca217f0d707f4ca844e96c55c43e34bc36fb12
Richard Gagg <r.gagg@outlook.com>
2025-01-15 18:59:39 +1100

    Completed while and repeat while loops Completed unique message and image loop

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
   @State private var imageName: String = ""
   @State private var imageNumber: Int = 0
+  @State private var lastImageNumber: Int = -1
   
   @State private var messageString: String = ""
-  @State private var messageIndex: Int = 0
+  @State private var lastMessageIndex: Int = -1
+  
   @State private var message: [String] = [
     "You Are Awesome!",
     "You Are Great!",
@@ -29,28 +31,50 @@ struct ContentView: View {
     
     ZStack {
       
+      LinearGradient(colors: [.blue, .clear],
+                     startPoint: .top,
+                     endPoint: .bottom)
+      .ignoresSafeArea()
+      
       VStack() {
+        
+        Text(messageString)
+          .font(.largeTitle)
+          .minimumScaleFactor(0.5)
+          .multilineTextAlignment(.center)
+          .foregroundStyle(.white)
+          .fontWeight(.heavy)
+          .shadow(color: .black, radius: 10, x: 10, y: 10)
+          .frame(height: 100)
+          .frame(maxWidth: .infinity)
+          .padding(.horizontal)
         
         Image(imageName)
           .resizable()
           .scaledToFit()
           .clipShape(RoundedRectangle(cornerRadius: 20))
-          .shadow(color: .gray, radius: 10, x: 10, y: 10)
-        
-        Spacer()
-        
-        Text(messageString)
-          .font(.largeTitle)
-          .multilineTextAlignment(.center)
-          .foregroundStyle(.red)
-          .fontWeight(.heavy)
+          .shadow(color: .black, radius: 20, x: 20, y: 20)
+          .padding()
         
         Spacer()
         
         Button {
-          messageString = (message.randomElement() ?? "")
+          var messageIndex: Int
+          var imageNumber: Int
           
-          imageName = "image\(Int.random(in: 0...9))"
+          repeat {
+            messageIndex = Int.random(in: 0...message.count - 1)
+          } while messageIndex == lastMessageIndex
+          
+          repeat {
+            imageNumber = Int.random(in: 0...9)
+          } while imageNumber == lastImageNumber
+          
+          lastMessageIndex = messageIndex
+          lastImageNumber = imageNumber
+          
+          messageString = (message[messageIndex])
+          imageName = "image\(imageNumber)"
           
         } label: {
           Text("Click Me!")
