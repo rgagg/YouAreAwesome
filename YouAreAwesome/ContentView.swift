1a07a07effa85d5bda3723de89c3dda18f7f3ca6
Richard Gagg <r.gagg@outlook.com>
2025-01-15 12:03:46 +1100

    Completed Randomization

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-  //  @State private var messageString = "I'm a programer!"
-  @State private var messageString = ""
   @State private var imageName: String = ""
+  @State private var imageNumber: Int = 0
+  
+  @State private var messageString: String = ""
+  @State private var messageIndex: Int = 0
+  @State private var message: [String] = [
+    "You Are Awesome!",
+    "You Are Great!",
+    "You Are Amazing!",
+    "You Are Incredible!",
+    "You Coding Genius!",
+    "You Are Amazing at Everything!"
+  ]
   
   var screenHeight: CGFloat { UIScreen.main.bounds.height }
   var screenWidth: CGFloat { UIScreen.main.bounds.width }
@@ -21,46 +31,32 @@ struct ContentView: View {
       
       VStack() {
         
-        Spacer()
-        
-        Image(systemName: imageName)
+        Image(imageName)
           .resizable()
           .scaledToFit()
-          .foregroundStyle(.orange)
-        //          .frame(width: 200, height: 200)
+          .clipShape(RoundedRectangle(cornerRadius: 20))
+          .shadow(color: .gray, radius: 10, x: 10, y: 10)
+        
+        Spacer()
         
         Text(messageString)
           .font(.largeTitle)
-          .fontWeight(.ultraLight)
-          .minimumScaleFactor(0.5)
           .multilineTextAlignment(.center)
           .foregroundStyle(.red)
-        //        .frame(width: 300, height: 150)
-          .frame(height: 150)
-          .frame(maxWidth: .infinity)
-          .padding()
+          .fontWeight(.heavy)
         
         Spacer()
         
         Button {
-          let message1 = "You Are Awesome!"
-          let message2 = "You Are Great!"
-          let image1 = "sun.max.fill"
-          let image2 = "hand.thumbsup"
+          messageString = (message.randomElement() ?? "")
           
-          if messageString == message1 {
-            messageString = message2
-            imageName = image1
-          } else {
-            messageString = message1
-            imageName = image2
-          }
+          imageName = "image\(Int.random(in: 0...9))"
+          
         } label: {
-          Text("Press Me!")
+          Text("Click Me!")
         }
         .buttonStyle(.borderedProminent)
         .font(.title2)
-        .tint(.orange)
       }
       .padding()
       
